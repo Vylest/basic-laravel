110c457b4eaca198f10454521d50ed2817b778f3
Eric Penner <epenner@unomaha.edu>
2016-05-19 21:02:10 +0000

    first commit

---
+++ database/migrations/2016_05_19_205610_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('body');
+            $table->integer('card_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('cards', function (Blueprint $table) {
+            $table->foreign('card_id')->references('id')->on('cards')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cards');
+    }
+}
